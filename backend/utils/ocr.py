4195ce1840d1c8591b3d042661745fe0b2813828
Sharmili <sharmiyuvaraj11@gmail.com>
2025-11-12 10:38:28 +0530

    Updated OCR backend and AdminUpload integration

--- backend/utils/ocr.py
+++ backend/utils/ocr.py
@@ -1,6 +1,9 @@
 import pytesseract
 from PIL import Image
 
+# ✅ Tell Python where Tesseract is installed
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Y.SHARMILI\OneDrive\Desktop\New folder (2)\Tesseract-OCR\tesseract.exe"
+
 def extract_text_from_image(image_path):
     """
     Extracts text from an image using Tesseract OCR.
@@ -12,4 +15,4 @@ def extract_text_from_image(image_path):
             text = "⚠ No readable text found in image."
         return text
     except Exception as e:
-        raise Exception(f"OCR extraction failed: {e}")
+        raise Exception(f"OCR extraction failed: {e}")
